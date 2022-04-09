24b8b4cf20bba54c474da4bb94461da8e76e88d9
Lau-Man-Ho-Mark <laumanho87@gmail.com>
2022-04-09 13:53:06 +0800

    Merged frag A, C]

--- GpPj/app/src/main/java/com/example/assignment2/ProgressFragment.java
+++ GpPj/app/src/main/java/com/example/assignment2/ProgressFragment.java
@@ -5,15 +5,22 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ProgressFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ProgressFragment extends Fragment {
+public class ProgressFragment extends Fragment implements View.OnClickListener{
+
+    ImageButton day1,day2,day3,day4,day6,day7,day8,day9,day11,day12,day13,day14,day16,day17,day18,day19,day21,day22,day23,day24,day26,day27,day28,day29,restday;
+    ArrayList<String> arr1 = new ArrayList<>();
+    ArrayList<String> arr2 = new ArrayList<>();
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -60,17 +67,24 @@ public class ProgressFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_progress, container, false);
+
+
         //Get the data
         //init(v);
-        /*
-        bundle = this.getArguments();
+
+        Bundle bundle = this.getArguments();
         if(bundle != null){
-            getData(bundle);
-            setData(bundle);
-            setGender(gender);
-            zodiacTv.setText(zodiacDetermination(forDay, forMonth));
-            zodiacImage(zodiacDetermination(forDay, forMonth));
-        }*/
+            Bundle FragABundle = bundle.getBundle("fragA");
+            arr1 = FragABundle.getStringArrayList("repList");
+            arr2 = FragABundle.getStringArrayList("burntCaloriesList");
+
+            for (String data: arr1)
+                System.out.println(data);
+
+            for (String data: arr2)
+                System.out.println(data);
+        }
+
 
 
         Log.d("Success", "progress fragment ready to leave");
@@ -79,5 +93,41 @@ public class ProgressFragment extends Fragment {
         //return inflater.inflate(R.layout.fragment_zodiac, container, false);
     }
 
+    public void init(View v){
+        day1=v.findViewById(R.id.day1);
+        day2=v.findViewById(R.id.day2);
+        day3=v.findViewById(R.id.day3);
+        day4=v.findViewById(R.id.day4);
+        day6=v.findViewById(R.id.day6);
+        day7=v.findViewById(R.id.day7);
+        day8=v.findViewById(R.id.day8);
+        day9=v.findViewById(R.id.day9);
+        day11=v.findViewById(R.id.day11);
+        day12=v.findViewById(R.id.day12);
+        day13=v.findViewById(R.id.day13);
+        day14=v.findViewById(R.id.day14);
+        day16=v.findViewById(R.id.day16);
+        day17=v.findViewById(R.id.day17);
+        day18=v.findViewById(R.id.day18);
+        day19=v.findViewById(R.id.day19);
+        day21=v.findViewById(R.id.day21);
+        day22=v.findViewById(R.id.day22);
+        day23=v.findViewById(R.id.day23);
+        day24=v.findViewById(R.id.day24);
+        day26=v.findViewById(R.id.day26);
+        day27=v.findViewById(R.id.day27);
+        day28=v.findViewById(R.id.day28);
+        day29=v.findViewById(R.id.day29);
+        restday=v.findViewById(R.id.day5);
+        restday=v.findViewById(R.id.day10);
+        restday=v.findViewById(R.id.day15);
+        restday=v.findViewById(R.id.day20);
+        restday=v.findViewById(R.id.day25);
+        restday=v.findViewById(R.id.day30);
+    }
 
+    @Override
+    public void onClick(View view) {
+
+    }
 }
